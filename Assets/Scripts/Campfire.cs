c8c725eb303ad1b0371a1727c856f10dfe689da3
Hae1385 <zx3248@naver.com>
2025-05-26 16:32:52 +0900

    붙여넣기

---
+++ Assets/Scripts/Campfire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour
+{
+    public int damage;
+    public float damageRate;
+
+    List<IDamageIbe> things = new List<IDamageIbe>();
+
+    private void Start()
+    {
+        InvokeRepeating("DealDamage", 0, damageRate);
+    }
+
+    void DealDamage()
+    {
+        for(int i = 0; i < things.Count; i++)
+        {
+            things[i].TakePhysiclaDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out IDamageIbe damagable))
+        {
+            things.Add(damagable);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.TryGetComponent(out IDamageIbe damagable))
+        {
+            things.Remove(damagable);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageIbe
+{
+    void TakePhysiclaDamage(int damage);
+}
+
+public class PlayerCondition : MonoBehaviour, IDamageIbe
+{
+    public UICondition uICondition;
+
+    Condition health { get { return uICondition.health; } }
+    Condition hunger { get { return uICondition.hunger; } }
+    Condition stamina { get { return uICondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    public event Action onTakeDamage;
+    private void Update()
+    {
+        hunger.Subject(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue <= 0f)
+        {
+            health.Subject(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amout)
+    {
+        health.Add(amout);
+    }
+
+    public void Eat(float amout)
+    {
+        hunger.Add(amout);
+    }
+    public void Die()
+    {
+        Debug.Log("die");
+    }
+
+    public void TakePhysiclaDamage(int damage)
+    {
+        health.Subject(damage);
+        onTakeDamage?.Invoke();
+    }
+
+    public bool UseStamina(float amount)
+    {
+        if(stamina.curValue -  amount < 0f)
+        {
+            return false;
+        }
+        stamina.Subject(amount);
+        return true;
+    }
+}
