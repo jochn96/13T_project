b82f603bc28d55efb91a5e7ace66c58760950681
wallyyoo <hyase22@naver.com>
2025-05-27 20:34:56 +0900

    fix : 디버그 로그 삭제

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -33,7 +33,7 @@ public class ResourceManager : MonoBehaviour
 
     public void AddResource(ResourceType type, int amount)
     {
-        Debug.Log("자원증가");
+     
         resources[type] += amount;
         UpdateUI();
     }
