efa88e4079f09d32a5164f2ee4e5f9a430eecc58
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-05-29 14:49:48 +0900

    Add : Weater 데이터 타입추가

--- Assets/Scripts/ScriptableObject/Data/Weather/Weatherdata.cs
+++ Assets/Scripts/ScriptableObject/Data/Weather/Weatherdata.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 
+public enum WeatherBuffType
+{
+    Health,
+    Stamina
+}
 public class WeatherDrop
 {
+    public string name;
     public GameObject DropPrefab;
     public int value;
+    public string weatherBuffType;
 }
 [CreateAssetMenu(fileName = "Weather", menuName = "New Weather")]
 public class Weatherdata : ScriptableObject
 {
     [Header("Info")]
     public string name;
+    public float value;
+    public WeatherBuffType weatherBuffType;
     public GameObject weaterPrefabs;
 }
